2178ba05d7a81651f733e5c63013c16d143f64fa
borgesj <jose.borges@border-innovation.com>
2021-12-03 17:07:28 +0000

    Desc. Order final

--- src/main/scala/Order.scala
+++ src/main/scala/Order.scala
@@ -1,4 +1,19 @@
 object Order {
-  def descendingOrder(num: Int): Int = ???
+  def descendingOrder(num: Int): Int = {
+    val numStr = num.toString
+    val aList : List[Any] = numStr.toList
+    val intList : List[Int] = aList.map(_.toString().toInt)
+    val orderedList : List[Int] = intList.sorted
+
+    val str = orderedList.mkString("")
+    str.toInt
+  }
+
+  def main(args: Array[String]): Unit = {
+
+    val randomInt = 132134556
+    print(descendingOrder(randomInt))
+
+  }
 
 }
